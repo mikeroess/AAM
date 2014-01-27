ddb2b8d2dc9aaffc68a15bef1e283864a301813e
mikeroess <mike.roess@gmail.com>
2014-01-27 14:22:28 -0500

    garden dianas done

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -135,4 +135,65 @@ class GalleriesController < ApplicationController
 
 	def icon_diana_19
 	end
+
+#garden diana controller
+    def garden_diana_1
+    end
+
+    def garden_diana_2
+    end
+
+    def garden_diana_3
+    end
+
+    def garden_diana_4
+    end
+
+    def garden_diana_5
+    end
+
+    def garden_diana_6
+    end
+
+    def garden_diana_7
+    end
+
+    def garden_diana_8
+    end
+
+    def garden_diana_9
+    end
+
+    def garden_diana_10
+    end
+
+    def garden_diana_11
+    end
+
+    def garden_diana_12
+    end
+
+    def garden_diana_13
+    end
+
+    def garden_diana_14
+    end
+
+    def garden_diana_15
+    end
+
+    def garden_diana_16
+    end
+
+    def garden_diana_17
+    end
+
+    def garden_diana_18
+    end
+
+    def garden_diana_19
+    end
+
+    def garden_diana_20
+    end
 end
