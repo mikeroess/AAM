0f372fd4fb7b375bbaa1d483ae4fe0c109df5d56
Jake Bell <jake@theunraveler.com>
2017-08-04 14:25:57 -0500

    Initial setup of magnifying glass functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require jquery.loupe
 //= require_tree .
 
 var $html = $('html');
--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,7 +1,9 @@
 class GalleriesController < ApplicationController
+  LOUPE_COOKIE_NAME = :gallery_loupe_notice_seen
+
   before_action :find_gallery
   before_action :find_template, only: :index
-  before_action :find_image, only: :show
+  before_action :find_image, :show_loupe_notice, only: :show
 
   rescue_from Gallery::NotFound, with: :not_found
 
@@ -31,5 +33,11 @@ class GalleriesController < ApplicationController
 
   def find_image
     @image = @gallery.images[params[:id].to_i - 1]
+  end
+
+  def show_loupe_notice
+    return if cookies.signed[LOUPE_COOKIE_NAME]
+    cookies.permanent.signed[LOUPE_COOKIE_NAME] = true
+    @show_loupe_notice = true
   end
 end
