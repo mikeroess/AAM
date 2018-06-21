27f464d2b7b470ac035f8ba4e57565b1a4fa15e8
Jake Bell <sigterm01@gmail.com>
2018-06-21 12:09:33 -0500

    Setting URL options for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,4 +73,6 @@ Ann::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_controller.default_url_options = { host: 'anneardenmcdonald.com', port: nil }
 end
