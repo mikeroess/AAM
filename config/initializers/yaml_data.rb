0dfe59289593e8ced0c3e50dafcd753f13b73262
Jake Bell <sigterm01@gmail.com>
2019-03-29 01:48:39 -0500

    Adding reloader functionality for yaml data directory

--- config/initializers/yaml_data.rb
+++ config/initializers/yaml_data.rb
@@ -2,18 +2,23 @@ require 'yaml'
 require 'pathname'
 require 'erb'
 
-Rails.application.config.to_prepare do
+data_dir = Rails.root.join('config', 'data')
+
+yaml_data_watcher = Rails.application.config.file_watcher.new([], data_dir.to_s => ['yml']) do
   {
     galleries: Gallery::Gallery,
     criticism: Criticism
   }.each_pair do |filename, model|
-    yaml = Pathname.new("#{Rails.application.paths['config'].existent.first}/data/#{filename}.yml")
+    yaml = data_dir.join("#{filename}.yml")
     raise "Could not load configuration. No such file - #{yaml}" unless yaml.exist?
 
     records = (YAML.load(ERB.new(yaml.read).result) || {}) || {}
     model.all = records.map.with_index do |attrs, index|
       attrs[:id] = index + 1 unless attrs['id']
       model.new(attrs)
-    end
+    end.freeze
   end
 end
+
+Rails.application.reloaders << yaml_data_watcher
+Rails.application.config.to_prepare { yaml_data_watcher.execute }
