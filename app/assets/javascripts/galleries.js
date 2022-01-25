04a9e33acee7adff2f361da682827a07a967d678
Jake Bell <sigterm01@gmail.com>
2022-01-25 17:54:39 -0600

    Fixing gallery scrolling on Safari

--- app/assets/javascripts/galleries.js
+++ app/assets/javascripts/galleries.js
@@ -5,12 +5,8 @@
   });
 
   var initPageScrolling = function initPageScrolling() {
-    var $html = $('html'),
-        // Firefox does not handle scrollTop with body very well, so we'll use
-        // $('html') for that. See http://stackoverflow.com/questions/8149155/animate-scrolltop-not-working-in-firefox
-        // for more.
-        $container = $html.hasClass('firefox') ? $html : $('body');
-    $container.on('click', '#scroll-up, #scroll-down', function(event) {
+    var $container = $(document.scrollingElement);
+    $(document).on('click', '#scroll-up, #scroll-down', function(event) {
       event.preventDefault();
       var new_position = $container.scrollTop() + ($(this).attr('id') == 'scroll-up' ? -300 : 300);
       $container.animate({scrollTop: new_position}, '500');
